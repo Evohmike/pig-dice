563ded91ce031e14d9ac7180cc139de1cb1e3066
evohmike <evohmike@gmail.com>
2018-05-15 08:50:58 +0300

    add all changes

---
+++ README.md
@@ -0,0 +1,38 @@
+# Ping-Pong
+#### This is a javascript application that uses probability to creat a simple game. 14 May 2018
+#### By **Evoh Mike**
+## Description
+Pig is a simple dice game first described in print by John Scarne in 1945.[1] As with many games of folk origin, Pig is played with many rule variations. Commercial variants of Pig include Pass the Pigs, Pig Dice,[2] and Skunk. Pig is commonly used by mathematics teachers to teach probability concepts.
+## Setup/Installation Requirements
+* JQuery and Bootstrap
+* javascript
+
+## Known Bugs/Updates/Corrections
+currently there is no bugs in this application.
+
+## Specifications
+The following are the specifications for this application:
+#### INPUT
+* The application only works when two players enter their names in the input areas provided.
+
+#### INPUT TYPE
+  * The application has a html5 input validation for text, it also requires you to enter text into the text boxes before you can proceed with playing the game.
+
+#### following are the rules of the game:
+  * Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold":
+  * If the player rolls a 1, they score nothing and it becomes the next player's turn.
+  * If the player rolls any other number, it is added to their turn total and the player's turn continues.
+  * If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn.
+* The first player to get 100 points or above wins the game.
+
+## Built Using
+* HTML5
+* CSS3
+* JQuery
+* Javascript
+## Contact details
+You can contact me at evohmike@gmail.com
+### License
+[License]
+*MIT*
+Copyright (c) 2018 **Evoh Mike**
--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,64 @@
 var player1 = "";
 var player2 = "";
 
-function player(name) {
+function Player(name) {
   this.name = name;
-  this.score =0;
-  this.diceFace=o;
-  this.total=0;
-
-
+  this.diceFace = 0;
+  this.score = 0;
+  this.total = 0;
 }
-function throwDice() {
-  return Math.floor(6*Math.random())+1;
 
+function diceThrow() {
+  return Math.floor(6 * Math.random()) + 1;
 }
-player.prototype.rollAOne = function() {
+
+Player.prototype.play = function() {
   if (this.diceFace === 1) {
     this.score = 0;
+    $('#playDie2').toggle();
+    $('#playDie1').toggle();
   } else {
     this.score += this.diceFace;
   }
 }
-player.prototype.hold = function(){
-  this.score = 0;
+
+Player.prototype.hold = function() {
   this.total += this.score;
+  this.score = 0;
 }
-player.prototype.winner = function() {
-  if (this.total>=100) {
-    alert(winner)
 
+Player.prototype.winner = function() {
+  if (this.total >= 100) {
+    alert("Congratulations " + this.name + " You are the winner");
   }
-
 }
+$(document).ready(function() {
+  $('form#register').submit(function(event) {
+    event.preventDefault();
+    var p1 = $('input#player1').val()
+    var p2 = $('input#player2').val()
+
+    player1 = new Player(p1);
+    player2 = new Player(p2);
+
+  });
+  $('#playDie1').click(function(event) {
+    event.preventDefault();
+    player1.diceFace = diceThrow();
+    $('#aRolled').empty()
+    $('#acurrentScore').empty();
+    $('#aRolled').append("You rolled  " + player1.diceFace);
+    player1.play();
+    $('#acurrentScore').append("Current scores is " + player1.score);
+  });
+  $('#playDie2').click(function(event) {
+    event.preventDefault();
+    player2.diceFace = diceThrow();
+    $('#bRolled').empty()
+    $('#bcurrentScore').empty();
+    $('#bRolled').append("You rolled  " + player2.diceFace);
+    player2.play();
+    $('#bcurrentScore').append("Current scores is " + player2.score);
+  });
+  
+});
